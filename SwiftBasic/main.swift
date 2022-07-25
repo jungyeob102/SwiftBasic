0f691a16a8d99ce06a3a2739ec0f353bd2546927
jylee-highbrow <asdf95102@naver.com>
2022-07-25 20:30:32 +0900

    [문제] 빙고

--- SwiftBasic/main.swift
+++ SwiftBasic/main.swift
@@ -8,27 +8,15 @@
 //import Foundation
 import Foundation
 
-var isDone: Bool = true
+let computerChoice: Int = Int.random(in: 1 ... 10)
+let myChoice = Int(readLine()!)!
 
-while isDone {
-    
-    let input = readLine()!
-    let temp = Int(input)
-//    let computer: Int = Int.random(in: 0 ... 2)
-    
-    if (temp == 0) {
-        print("바위")
-        isDone = false
-    }
-    else if (temp == 1) {
-        print("가위")
-        isDone = false
-    }
-    else if (temp == 2) {
-        print("보")
-        isDone = false
-    }
-    else {
-        print("일치하지 않습니다.")
-    }
+if (computerChoice > myChoice) {
+    print("Up")
+}
+else if (computerChoice == myChoice) {
+    print("Same")
+}
+else if (computerChoice < myChoice) {
+    print("Down")
 }
