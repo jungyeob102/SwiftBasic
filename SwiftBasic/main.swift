e66e37b609644608a4c51c83a507bf46a440a93f
jylee-highbrow <asdf95102@naver.com>
2022-07-25 20:15:31 +0900

    [문제] #가위바위보

--- SwiftBasic/main.swift
+++ SwiftBasic/main.swift
@@ -8,4 +8,27 @@
 //import Foundation
 import Foundation
 
-print("Hello, World!")
+var isDone: Bool = true
+
+while isDone {
+    
+    let input = readLine()!
+    let temp = Int(input)
+//    let computer: Int = Int.random(in: 0 ... 2)
+    
+    if (temp == 0) {
+        print("바위")
+        isDone = false
+    }
+    else if (temp == 1) {
+        print("가위")
+        isDone = false
+    }
+    else if (temp == 2) {
+        print("보")
+        isDone = false
+    }
+    else {
+        print("일치하지 않습니다.")
+    }
+}
