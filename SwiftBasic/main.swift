c72955c491da774053daa115b3d080404d0ebb24
jylee-highbrow <asdf95102@naver.com>
2022-07-26 23:35:37 +0900

    [문법] #문자열 쪼개기 - 문자열을 배열로 변환(2)

--- SwiftBasic/main.swift
+++ SwiftBasic/main.swift
@@ -9,15 +9,10 @@
 import Foundation
 
 //문자열 배열로 변경
-//1. 배열 생성 후 append 이용
+//1. 문자열을 배열 타입으로 변경
 
 //문자 입력받기
 let alphabet = readLine()!
 
-//문자 배열 생성 (빈 값)
-var arr = [Character]()
-
-//반복문을 통한 문자 입력
-for i in alphabet {
-    arr.append(i)
-}
+//문자열을 배열 타입으로 변환
+var arr = Array(alphabet)
