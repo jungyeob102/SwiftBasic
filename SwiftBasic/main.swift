42615ce9154ba9c21ed8357387f1f21c9f899f29
jylee-highbrow <asdf95102@naver.com>
2022-07-26 23:33:37 +0900

    [문법] #문자열 쪼개기 - 문자열을 배열로 변환 (1)

--- SwiftBasic/main.swift
+++ SwiftBasic/main.swift
@@ -8,15 +8,16 @@
 //import Foundation
 import Foundation
 
-let computerChoice: Int = Int.random(in: 1 ... 10)
-let myChoice = Int(readLine()!)!
+//문자열 배열로 변경
+//1. 배열 생성 후 append 이용
 
-if (computerChoice > myChoice) {
-    print("Up")
-}
-else if (computerChoice == myChoice) {
-    print("Same")
-}
-else if (computerChoice < myChoice) {
-    print("Down")
+//문자 입력받기
+let alphabet = readLine()!
+
+//문자 배열 생성 (빈 값)
+var arr = [Character]()
+
+//반복문을 통한 문자 입력
+for i in alphabet {
+    arr.append(i)
 }
