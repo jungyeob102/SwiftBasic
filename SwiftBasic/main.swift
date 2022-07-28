a8ef824894d1761160f5b6ae5960de2303c751bf
jylee-highbrow <asdf95102@naver.com>
2022-07-29 00:09:23 +0900

    [문제] #팩토리얼 구현

--- SwiftBasic/main.swift
+++ SwiftBasic/main.swift
@@ -1,18 +1,17 @@
 import Foundation
 
 //연습 문제
-//소수 판별
+//팩토리얼
 
-func isPrimeNumber(num : Int) -> () {
-    for i in 2 ..< num {
-        if (num % i == 0) {
-            print("소수가 아닙니다.")
-            return
-        }
+func factorial(_ num : Int) -> (Int) {
+
+    var result = 1
+
+    for i in 1 ... num {
+        result *= i
     }
     
-    print("소수입니다.")
+    return result
 }
 
-var inputData = Int(readLine()!)!
-isPrimeNumber(num: inputData)
+print(factorial(5))
