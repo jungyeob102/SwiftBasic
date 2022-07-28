ed0a5bb264bd47735e3c77f2bc493294d6f92a02
jylee-highbrow <asdf95102@naver.com>
2022-07-28 13:36:48 +0900

    [문제] #입력받은 문자열 중 랜덤의 문자를 출력하는 함수

--- SwiftBasic/main.swift
+++ SwiftBasic/main.swift
@@ -1,17 +1,19 @@
 import Foundation
 
-//inout 키워드를 사용하면 메모리 주소를 참조
-//상수가 아닌 변수로 저장
-func swap (a: inout Int, b: inout Int)
-{
-    let temp = a
-    a = b
-    b = temp
+//연습 문제
+//입력받은 문자열 중 랜덤의 문자를 출력하는 함수
+
+@discardableResult
+func randomCharacter(temp : String) -> Character {
+//    let arrTemp = Array(temp)
+//    let randomIndex = Int.random(in: 0 ..< arrTemp.count)
+//    print(randomIndex)
+//    
+    let randomChar = temp.randomElement()!
+    
+    return randomChar
 }
 
-var temp1: Int = 1
-var temp2: Int = 2
-swap(a: &temp1, b: &temp2)
+var inputData = readLine()!
 
-print(temp1)
-print(temp2)
+print(randomCharacter(temp: inputData))
