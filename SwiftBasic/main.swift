011a4110308a849b9277d1b20a84caef83537a64
jylee-highbrow <asdf95102@naver.com>
2022-07-31 23:40:05 +0900

    [문제] #가위바위보 안정성 추가

--- SwiftBasic/main.swift
+++ SwiftBasic/main.swift
@@ -1,14 +1,29 @@
 import Foundation
 
-//연습 문제
-//팩토리얼
-
-func factorial(_ num : Int) -> (Int) {
-    print(num)
-    if num <= 1 {
-        return 1
+var isBreak: Bool = true
+while isBreak {
+    let input: String = readLine()!
+    
+    if Int(input) != nil {
+        let result: Int = Int(input)!
+    
+        if result == 0 {
+            print("바위")
+            isBreak = false
+        }
+        else if result == 1 {
+            print("가위")
+            isBreak = false
+        }
+        else if result == 2 {
+            print("보")
+            isBreak = false
+        }
+        else {
+            print("다시 입력하시오.")
+        }
     }
-    return num * factorial(num - 1)
+    else {
+        print("숫자를 입력해주세요.")
+    }
 }
-
-print(factorial(10))
