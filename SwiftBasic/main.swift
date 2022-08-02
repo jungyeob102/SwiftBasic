cf5efd3ba1d26889b354850ba11f4a06ae698c03
jylee-highbrow <asdf95102@naver.com>
2022-08-02 23:26:16 +0900

    [문법] 구조체 및 타입 프로퍼티

--- SwiftBasic/main.swift
+++ SwiftBasic/main.swift
@@ -1,25 +1,29 @@
 import Foundation
 
-var arr: [Int] = []
-var maxNum: Int = 0
-var maxNumIndex: Int = 0
-
-for _ in 1 ... 9 {
-    let comp: String = readLine()!
+struct Sample {
+    //인스턴스 프로퍼티
+    var age: Int = 28
+    var name: String = "이중엽"
     
-    if Int(comp) != nil {
-        arr.append(Int(comp)!)
+    //타입 프로퍼티
+    //저장 타입 프로퍼티는 상수, 변수 가능
+    //연산 타입 프로퍼티는 상수로만 가능
+    //타입 프로퍼티는 인스턴스의 생성 여부와 상관없이 타입 프로퍼티 자체로 값이 하나로 존재한다.
+    //즉, 해당 타입의 모든 인스턴스가 공용으로 사용할 수 있다.
+    static let nick: String = "dodo"
+    
+    //인스턴트 메소드
+    func sayName() {
+        print("안녕하세요 \(name) 입니다.")
+    }
+    
+    //타입 메소드
+    static func sayName() {
+        print("안녕하세요 \(nick) 입니다.!")
+        print(">???")
     }
 }
 
-let count = arr.count
+//var temp: Sample = Sample()
 
-for i in 0 ..< count {
-    if arr[i] > maxNum {
-        maxNum = arr[i]
-        maxNumIndex = i + 1
-    }
-}
-
-print(maxNum)
-print(maxNumIndex)
+print(Sample.sayName())
