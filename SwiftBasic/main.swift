faba1e6d5989360d32e81f28bee7f0a64acc888e
jungyeob <asdf95102@naver.com>
2022-08-11 17:02:55 +0900

    [문법] #map 및 closure 연습

--- SwiftBasic/main.swift
+++ SwiftBasic/main.swift
@@ -1,25 +1,18 @@
 import Foundation
 
-var arrTemp: Array<Int> = [1,2,3,4,5,6]
-let temp = arrTemp.sorted{ $0 > $1 }
+let numbers: [Int] = [0,1,2,3,4]
 
+var doubleNumbers: [Int] = numbers.map({ (number: Int) -> Int in return number * 2})
+print(doubleNumbers)
 
-func makeIncrementer(forIncrement amount: Int) -> (() -> Int) {
-    var runningTotal = 0
-    func incrementer() -> Int {
-        runningTotal += amount
-        return runningTotal
-    }
-    return incrementer
-}
+var doubleNumbers2: [Int] = numbers.map({return $0 * 2})
+print(doubleNumbers2)
 
-let incrementByTwo: (() -> Int) = makeIncrementer(forIncrement: 2)
+var doubleNumbers3: [Int] = numbers.map() {return $0 * 2}
+print(doubleNumbers3)
 
-let first: Int = incrementByTwo()
-print(first)
+var doubleNumbers4: [Int] = numbers.map() {$0 * 2}
+print(doubleNumbers4)
 
-let second: Int = incrementByTwo()
-print(second)
-
-//클로저와 함수는 참조 타입!
-//따라서 클로저와 함수를 담은 상수 또는 변수를 다른 상수 또는 변수에 할당한다면 두 상수가 모두 같은 클로저 또는 함수를 가리킨다.
+var doubleNumbers5: [Int] = numbers.map{$0 * 2}
+print(doubleNumbers5)
